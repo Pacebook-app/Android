58f92f2f64e1d6b6a8802477e1e318c7289b6a07
장기준 <autojjangs@naver.com>
2025-08-25 16:56:52 +0900

    [기준/Merge] pull 받기 전 중간저장

--- app/src/main/java/com/example/pacebook/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pacebook/ui/main/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.pacebook.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pacebook.R
+import com.example.pacebook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //맨 처음 화면을 보여줄 때, HomeFragment Default
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, RunningInitialFragment())
+                .commit()
+        }
+
+        binding.mainBnv.setOnClickListener { item ->
+            when (item.itemId) {
+                R.id.   
+            }
+        }
     }
 }
