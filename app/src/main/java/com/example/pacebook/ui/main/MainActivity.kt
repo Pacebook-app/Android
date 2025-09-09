9b256740c716a002a13476605238538c3ef3a877
장기준 <autojjangs@naver.com>
2025-09-09 22:53:54 +0900

    [기준/UI, Logic] - BottomNavigation 동작 구현

--- app/src/main/java/com/example/pacebook/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pacebook/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pacebook.R
 import com.example.pacebook.databinding.ActivityMainBinding
+import com.example.pacebook.ui.community.CommunityFragment
+import com.example.pacebook.ui.mypage.MypageFragment
 import com.naver.maps.map.NaverMapSdk
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +20,38 @@ class MainActivity : AppCompatActivity() {
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NcpKeyClient("d2nb10oq0w")
 
+        // 첫 화면은 러닝(지도) 화면으로
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.main, RunningmateFragment())
-                .commit()
-        }
-
-
-
-
-        //맨 처음 화면을 보여줄 때, HomeFragment Default
-        if(savedInstanceState == null){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_fragment_container, RunningInitialFragment())
                 .commit()
+            binding.mainBnv.selectedItemId = R.id.runningFragment
+        }
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.communityFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, CommunityFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.runningFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, RunningInitialFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.mypageFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, MypageFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
         }
 
 
